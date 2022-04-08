85280c580c6bcbd86c2bded79568009e2beec129
Vladimir Lakhtin <99466577+VladimirLakhtin@users.noreply.github.com>
2022-04-08 13:37:25 +0300

    hide bot's token

--- insta_pred_bot/create_bot.py
+++ insta_pred_bot/create_bot.py
@@ -4,6 +4,6 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 storage = MemoryStorage()
 
-bot = Bot(token='5200980757:AAG4yNGnlgvDNiRoHeXU9P-4qEFC3GDHFQ8')
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
